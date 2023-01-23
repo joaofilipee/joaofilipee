872413ac7ac1b77df56736e96a81840d2f1011a2
João Filipe <joao1filipe@outlook.com>
2023-01-23 10:17:56 -0300

    starting home

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import './App.css';
 // components
 import Navbar from './components/Navbar';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Navbar />
-        
+        <Routes>
+          <Route path='/' element={<Home />}/>
+        </Routes>
       </BrowserRouter>
     </div>
   );
---
+++ src/components/Home.js
@@ -0,0 +1,19 @@
+import "./Home.css"
+
+const Home = () => {
+  return (
+    <div>
+      <div className="presentation">
+        <h2 className="about">
+          Olá, eu sou um desenvolvedor Front end
+        </h2>
+        <p className="description">
+          Desenvolvo aplicações web através de código
+        </p>
+      </div>
+      
+    </div>
+  )
+}
+
+export default Home
