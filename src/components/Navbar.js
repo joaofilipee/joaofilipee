2e9531ce2e9a85ead06655c6a4a4bfb4f4f53e53
João Filipe <joao1filipe@outlook.com>
2023-01-23 09:27:00 -0300

    initial commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,20 @@
+import "./Navbar.css"
+import { Link, NavLink } from "react-router-dom"
+
+const Navbar = () => {
+  return (
+    <nav className="nav-bar">
+        <div className="name">
+            <NavLink to="/">J<span className="red">o</span>ão</NavLink>
+        </div>
+        <div className="items">
+        <NavLink>Home</NavLink>
+        <NavLink>Projetos</NavLink>
+        <NavLink>Contato</NavLink>
+        <NavLink>Skills</NavLink>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
