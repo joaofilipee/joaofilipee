9965fdf09282b90593a470af1271494957606210
João Filipe <joao1filipe@outlook.com>
2023-01-27 16:57:21 -0300

    starting responsive design

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,18 +1,51 @@
 import "./Navbar.css"
-import { Link, NavLink } from "react-router-dom"
+
+// hooks
+import { NavLink } from "react-router-dom"
+
+
+// icons
+import { AiOutlineMenu } from "react-icons/ai"
 
 const Navbar = () => {
   return (
     <nav className="nav-bar">
-        <div className="name">
-            <NavLink to="/">J<span className="red">o</span>ão</NavLink>
+        <div className="computer-navbar">
+          <div className="name">
+              <NavLink to="/">J<span className="red">o</span>ão</NavLink>
+          </div>
+          <div className="items">
+            <NavLink to="/">Home</NavLink>
+            <a href="#skills">Skills</a>
+            <NavLink to="/projects">Projetos</NavLink>
+            <a href="#contact">Contato</a>
+          </div>
         </div>
-        <div className="items">
-        <NavLink to="/">Home</NavLink>
-        <a href="#skills">Skills</a>
-        <NavLink to="/projects">Projetos</NavLink>
-        <a href="#contact">Contato</a>
+
+        <div className="mobile-navbar">
+
+          <div className="navbar-content">
+            <div className="name">
+                <NavLink to="/">J<span className="red">o</span>ão</NavLink>
+            </div>
+            <div>
+                <AiOutlineMenu className="menu" />
+            </div>
+          </div>
+
+          <div className="mobile-navbar-links">
+
+            <div className="mobile-items">
+                <NavLink to="/"> <span className="red">H</span>ome</NavLink>
+                <a href="#skills"><span className="red">S</span>kills</a>
+                <NavLink to="/projects"><span className="red">P</span>rojetos</NavLink>
+                <a href="#contact"><span className="red">C</span>ontato</a>
+            </div>
+
+          </div>
+          
         </div>
+
     </nav>
   )
 }
